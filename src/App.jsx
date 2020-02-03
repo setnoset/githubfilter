bde1b2a66bcf4f90a038e90856762437d3af1540
Lucas Schwendler Vieira <lucas.dsv@hotmail.com>
2020-02-03 04:32:48 -0300

    Create UI skeleton with RepositoryFilter as base component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-const element = (
-	<h1>React running</h1>
-);
+class LanguageSelector extends React.Component {
+	render() {
+		return (
+			<h2>Select language</h2>
+		);
+	}
+}
+
+class RepositoryList extends React.Component {
+	render() {
+		return (
+			<h2>View filtered repositories</h2>
+		);
+	}
+}
+
+class RepositoryFilter extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Repository Filter</h1>
+				<LanguageSelector/>
+				<RepositoryList/>
+			</React.Fragment>
+		);
+	}
+}
+
+const element = <RepositoryFilter/>
 
 ReactDOM.render(element, document.getElementById('content'));
